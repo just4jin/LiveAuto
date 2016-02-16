05c5c427726f471ad767c1febee0cf89cca85962
Just4jin <just4jin@users.noreply.github.com>
2016-02-15 22:57:17 -0500

    Update db_query.py

--- db_query.py
+++ db_query.py
@@ -2,6 +2,12 @@
 
 import cx_Oracle
 
+def db_connect(localhost = "******", port = ****, sid = '******' ):
+	dsn_tns = cx_Oracle.makedsn(localhost, port, sid)
+	conn = cx_Oracle.connect('****', '****', dsn_tns)
+	c = conn.cursor()
+	return conn, c
+	
 def exe_query(cursor, query):
 	try:
 		cursor.execute(query)
@@ -17,12 +23,6 @@ def read_query(cursor, query):
 	names = [x[0] for x in cursor.description]
 	rows=cursor.fetchall()
 	return pd.DataFrame(rows, columns=names)
-	
-def db_connect(localhost = "172.30.80.107", port = 1521, sid = 'NEWMETRICS' ):
-	dsn_tns = cx_Oracle.makedsn(localhost, port, sid)
-	conn = cx_Oracle.connect('perf_indicators', 'perf_indicators', dsn_tns)
-	c = conn.cursor()
-	return conn, c
 	
 def read_db(db_taer_query):
 	conn, c = db_connect()
